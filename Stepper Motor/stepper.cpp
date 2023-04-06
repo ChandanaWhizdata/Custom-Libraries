75f8205547e9ecde54ca8b072741443cfac6418e
ChandanaWhizdata <chandanar@entrix.in>
2023-04-06 12:36:35 +0530

    created a header and source files for stepper motor control

---
+++ Stepper Motor/stepper.cpp
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ Stepper Motor Control Source file 
+
+  Company:
+    Whizdata Solutions LLP.
+
+  File Name:
+    stepper.c
+
+  Summary:
+    This file contains all the functions related to stepper motor control
+
+  Description:
+    This file contains implementation of the stepper control functions
+ 
+ *******************************************************************************/
---
+++ Stepper Motor/stepper.h
@@ -0,0 +1,13 @@
+/*******************************************************************************
+  Stepper Motor System Definitions
+
+  File Name:
+    stepper.h
+
+  Summary:
+    Header file which shares global variables and function prototypes.
+
+  Description:
+    This file contains the global variables and function prototypes for a stepper motor control project.
+
+ *******************************************************************************/
