04b4af0a1db441d758cc65d84541523f3b1be07e
ChandanaWhizdata <chandanar@entrix.in>
2023-04-10 15:47:01 +0530

    TFT display custom library added

---
+++ TFT Display/320 X 240/tft_display.cpp
@@ -0,0 +1,36 @@
+/*******************************************************************************
+  TFT Display Source file 
+
+  Company:
+    Whizdata Solutions LLP.
+
+  File Name:
+    tft_display.cpp
+
+  Summary:
+    This file contains all the functions related to TFT display control
+
+  Description:
+    This file contains implementation of the display
+ 
+ *******************************************************************************/
+
+/*******************************************************************************
+Header Inclusions
+*******************************************************************************/
+#include <tft_display.h>
+
+/*******************************************************************************
+Functions
+*******************************************************************************/
+
+/******************************************************************************
+Function:     TFT_Init
+Description:  TFT display initialization
+Input:        nothing (uses some global variables)
+Output:       nothing (modifies some global variables)            
+******************************************************************************/
+void TFT_Init(void)
+{
+    
+}
---
+++ TFT Display/320 X 240/tft_display.h
@@ -0,0 +1,24 @@
+/*******************************************************************************
+  System Definitions
+
+  File Name:
+    tft_display.h
+
+  Summary:
+    Header file which shares global variables and function prototypes.
+
+  Description:
+    This file contains the global variables and function prototypes for a TFT display control.
+
+ *******************************************************************************/
+
+// ensure this library description is only included once
+#ifndef TFT_DISPLAY_H
+#define TFT_DISPLAY_H
+
+/*******************************************************************************
+Function prototypes
+*******************************************************************************/
+void TFT_Init(void);
+
+#endif  //TFT_DISPLAY_H
